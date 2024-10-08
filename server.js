c7035b4239c2515959f48948233c85c02867e977
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2024-10-08 09:15:11 +0100

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { updateEntries, handleClarifaiRequest } from "./controllers/image.js";
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-clean-28431",
     user: "arthurogunfuye",
     password: "",
     database: "smart-brain",
